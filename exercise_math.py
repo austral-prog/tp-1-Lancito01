c127a18354c76ba4c532e1d1f38c011322d9ec9d
Andy <andy@lancito01.dev>
2025-08-08 13:00:49 -0300

    Realizado el tp

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,13 @@
 def math():
     a = 57
     b = 7
+
+    print(a + b) # suma
+    print(a - b) # resta
+    print(a * b) # multiplicacion
+    print((a + b) / 2) # promedio
+    print(a // b) # division entera
+    print(a % b) # resto
+    print(a / b) # division
+
+math()
